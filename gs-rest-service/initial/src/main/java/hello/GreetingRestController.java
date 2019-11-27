7ecfe79839c0d1c787d7c5e3659b0ec626369ab4
Tiago Póvoa <tiago.quin@gmail.com>
2019-11-27 10:53:18 +0100

    add a route that gives a token for free :D

--- gs-rest-service/initial/src/main/java/hello/GreetingRestController.java
+++ gs-rest-service/initial/src/main/java/hello/GreetingRestController.java
@@ -1,6 +1,13 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,5 +23,27 @@ public class GreetingRestController {
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
+    }
+
+    @RequestMapping(value = "/freejwt", method = RequestMethod.GET)
+    public ResponseEntity<String> freejwt(@RequestParam(value="name", defaultValue="World") String name) {
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("wow very secret");
+            String token = JWT.create()
+                    .withIssuer("I did it")
+                    .withClaim("Bob", "Jean")
+                    .withClaim("counter", counter.incrementAndGet())
+                    .sign(algorithm);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", token);
+
+            return new ResponseEntity<String>("Bonjour", headers, HttpStatus.OK);
+
+
+        } catch (JWTCreationException exception){
+            return new ResponseEntity<String>("Non!", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
